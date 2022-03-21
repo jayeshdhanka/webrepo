361ca03233bbcca97972d75a463b1f0d2cdf903f
jayeshdhanka <jayesh.dhanka@covrize.com>
2022-03-21 12:49:37 +0530

    About and home page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About">
+            <p>Hi I'm the prod creator of this site, which I built with Gats.by</p>
+        </Layout>
+    )
+}
+
+export default AboutPage
